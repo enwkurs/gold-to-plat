9278ef7ce4d83881eb7c853bde03d57b82c2c8a2
enwkurs <erinwajca@gmail.com>
2025-10-14 13:35:58 +0200

    number letters

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // Convert Gold to Plat --------------------------------------------
 
+function easyNum(num) {
+    if (num >= 1e12) return (num / 1e12).toFixed(1).replace(/\.0$/, '') + 'T gold';
+    if (num >= 1e9)  return (num / 1e9).toFixed(1).replace(/\.0$/, '') + 'B gold';
+    if (num >= 1e6)  return (num / 1e6).toFixed(1).replace(/\.0$/, '') + 'M gold';
+    if (num >= 1e3)  return (num / 1e3).toFixed(1).replace(/\.0$/, '') + 'K gold';
+    return num.toString();
+}
+
 const goldInput = document.querySelector("#goldInput");
+const easyGold = document.querySelector("#easyGold");
 const platOutput = document.querySelector("#platOutput");
-console.log(`[Found] ${goldInput} ${platOutput}`);
+console.log(`[Found] ${goldInput} ${platOutput} ${easyGold}`);
 
 function goldToPlat() {
     console.log("Typing happened");
 
     const goldValue = Number(goldInput.value);
     console.log(`Const goldValue Number = ${goldValue}`);
+
+    easyGold.textContent = easyNum(goldValue);
 
     const platValue = goldValue / 10000000;
     console.log(`plat Value is ${platValue}`);
@@ -18,6 +29,8 @@ function goldToPlat() {
     } else {
         platOutput.textContent = "";
     }
+
+    
 }
 
 
